bedaf0a8c92dad279128736c2ea1ddf257940cce
Brendan Finan <brendanfinan@protonmail.com>
2026-03-26 21:58:02 -0500

    Live on the Chrome Web Store

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/1MC",
+        destination:
+          "https://docs.google.com/presentation/d/1I4W6cfLPrOS3EVM0a8EVuYObCpFBDEyI8ycMC8HFgVk/edit?usp=sharing",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
---
+++ tools/dev/notes/PUBLISHING.md
@@ -0,0 +1,7 @@
+# Notes on Publishing
+
+## Chrome Web Store
+
+[Slopblock Zero on CWS](https://chromewebstore.google.com/detail/slopblock-zero/fghjlgmoofbfekdhldaandocejfdjoic)
+
+
